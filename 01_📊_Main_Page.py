69ba696c6bdc8723a9db8f998f59e7c05d446bbe
Ricardo Júnior <95833355+Ricardo661@users.noreply.github.com>
2023-10-25 17:08:02 -0300

    Update 01_📊_Main_Page.py

--- 01_📊_Main_Page.py
+++ 01_📊_Main_Page.py
@@ -180,9 +180,10 @@ with st.container():
         aux = df2.loc[:, 'city'].nunique()
         col3.metric('Cidades Cadastrados', aux)
     with col4:
-        locale.setlocale(locale.LC_ALL, 'pt_BR')
+        #locale.setlocale(locale.LC_ALL, 'pt_BR') 
         aux = df2.loc[:, 'votes'].sum()
-        formatted_aux = locale.format('%0.2f', aux, grouping=True)
+        formatted_aux = "{:,.2f}".format(aux).replace(",", " ")
+        #formatted_aux = locale.format('%0.2f', aux, grouping=True)
         col4.metric('Avaliações feitas na Plataforma', formatted_aux)
     with col5:
         aux = df2.loc[:, 'cuisines_number'].nunique()
